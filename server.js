0203cc5cdd80497ea340d1672bf893f60edf75da
Kazihehehe <kazisulemanislam@gmail.com>
2025-08-10 01:15:33 +0530

    Update server.js
    Signed-off-by: Kazihehehe <kazisulemanislam@gmail.com>

--- server.js
+++ server.js
@@ -1,32 +1,27 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
-
-// Create server
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname)));
 
-// Login route
+// Routes
 app.post('/login', (req, res) => {
   const { email, password } = req.body;
+  
   if (email === "piyaa@example.com" && password === "18party") {
-    return res.redirect('/home.html');
+    return res.json({ success: true, redirect: '/home.html' });
   }
-  return res.status(401).send('Invalid credentials');
+  
+  return res.status(401).json({ success: false, message: 'Invalid credentials' });
 });
 
-// Serve HTML files
-app.get('/home.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'home.html'));
-});
-
-// Catch-all route
+// Serve all HTML files
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, req.path));
 });
 
 // Start server
