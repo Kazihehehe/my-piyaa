00ecae70a25c8ca955558333c3a6165234c182bf
Kazihehehe <kazisulemanislam@gmail.com>
2025-08-11 11:59:25 +0530

    Update server.js
    Signed-off-by: Kazihehehe <kazisulemanislam@gmail.com>

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(express.static(path.join(__dirname), {
       res.setHeader('Cache-Control', 'no-cache');
     }
   }
-});
+}));
 
 // Enhanced login endpoint
 app.post('/api/login', (req, res) => {
